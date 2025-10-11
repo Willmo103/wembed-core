c5ff484c5b2ecb500318a4e51872461246ebd06d
WIll Morris <88165849+Willmo103@users.noreply.github.com>
2025-10-10 23:15:24 -0500

    Enhance ListBuilder with new modes and options; add configuration and logging models

--- src/wembed_core/file_scanner/list_builder.py
+++ src/wembed_core/file_scanner/list_builder.py
@@ -1,26 +1,85 @@
+from enum import Enum
 from pathlib import Path
 from typing import List, Optional, Set
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, computed_field
 
-from wembed_core.constants import IGNORE_EXTENSIONS, IGNORE_PARTS
+from wembed_core.constants import (
+    IGNORE_EXTENSIONS,
+    IGNORE_PARTS,
+    OBSIDIAN_MARKER,
+    REPO_MARKER,
+)
+
+
+class ListBuilderModes(str, Enum):
+    """Defines modes for ListBuilder operation."""
+
+    FULL: str = Field(
+        default="full",
+        description="Full scan of the filesystem for all markers."
+        " Uses all configured roots in the ~/wembed/config.json:scan_root_paths section.",
+        frozen=True,
+    )
+    REPOSITORY: str = Field(
+        default="repo",
+        description="Scan for multiple repository folders for repository indexing.",
+        frozen=True,
+    )
+    OBSIDIAN: str = Field(
+        default="obsidian",
+        description="Scan for multiple Obsidian vault folders for Obsidian indexing.",
+        frozen=True,
+    )
+    PROJECT: str = Field(
+        default="project",
+        description="Scan inside of a project folder using git ls-files to find files as the default ignoring configuration.",
+        frozen=True,
+    )
 
 
 class ListBuilderOptions(BaseModel):
+    """
+    ListBuilderOptions defines configuration options for building a list of files
+    to be processed. It includes parameters for scanning mode, root path, file
+    matching patterns, subdirectory filtering, ignored extensions and path parts,
+    recursion, and parent folder markers.
+
+    Attributes:
+        mode (str): Mode of operation
+        root_path (Optional[Path]): Root directory to start scanning from.
+        match_patterns (Optional[List[str]]): List of glob patterns to match files.
+        subdirs (Optional[List[str]]): List of subdirectories to limit results to.
+        ignored_extensions (Optional[Set[str]]): Set of file extensions to exclude.
+        ignored_file_parts (Optional[Set[str]]): Set of path segments to exclude.
+        recursive (Optional[bool]): Whether to scan directories recursively.
+        parent_folder_markers (Optional[List[str]]): List of parent folder markers to include.
+    """
+
+    mode: str = Field(
+        default=None,
+        description="Mode of operation: 'git' for git-tracked files, 'fs' for filesystem scan.",
+    )
     root_path: Optional[Path] = Field(
         default=Path.cwd(), description="Root directory to start scanning from."
     )
     match_patterns: Optional[List[str]] = Field(
         default=None, description="List of glob patterns to match files."
     )
-    patterns: Optional[List[str]] = Field(
-        default=None, description="List of glob patterns to match files."
+    subdirs: Optional[List[str]] = Field(
+        default=None, description="List of subdirectories to limit results to."
     )
-    exclude_ext: Optional[Set[str]] = Field(
+    ignored_extensions: Optional[Set[str]] = Field(
         default=IGNORE_EXTENSIONS, description="Set of file extensions to exclude."
     )
-    exclude_parts: Optional[Set[str]] = Field(
+    ignored_file_parts: Optional[Set[str]] = Field(
         default=IGNORE_PARTS, description="Set of path segments to exclude."
+    )
+    recursive: Optional[bool] = Field(
+        default=True, description="Whether to scan directories recursively."
+    )
+    parent_folder_markers: Optional[List[str]] = Field(
+        default=None, description="List of parent folder markers to include."
     )
 
 
